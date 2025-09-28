2749e7901b16d00341f13e527a795a7a0e94f850
Mokassem0 <107583487+Mokassem0@users.noreply.github.com>
2025-09-28 13:56:47 +0300

    add BestSellerListViewItem part1

--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/utils/assets.dart';
 import 'package:flutter_application_1/core/utils/font.dart';
 import 'package:flutter_application_1/features/home/presentation/views/widget/custom_app_bar.dart';
 import 'package:flutter_application_1/features/home/presentation/views/widget/featured_book_list_view.dart';
@@ -20,8 +21,36 @@ class HomeViewBody extends StatelessWidget {
       child: Text("Best Seller",
       style: Font.titleMidium,
       ),
-    ),)
+    ),),
+    BestSellerListViewItem(),
     ]);
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  SizedBox(
+      height: 125,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 14),
+        child: Row(
+          children: [
+            AspectRatio(aspectRatio: 2.5/4,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsImg.test))
+              ),
+            ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
