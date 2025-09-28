aba9098a277d2ede9cd1f67bbae3a1041dc81096
Mokassem0 <107583487+Mokassem0@users.noreply.github.com>
2025-09-28 12:38:57 +0300

    add feature list view to home

--- lib/features/home/presentation/views/widget/custom_list_view_item.dart
+++ lib/features/home/presentation/views/widget/custom_list_view_item.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/core/utils/assets.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({Key? key}) : super(key: key);
+class FeatureListViewItem extends StatelessWidget {
+  const FeatureListViewItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.28,
-      child: AspectRatio(aspectRatio: 2.7/4,
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            fit: BoxFit.fill,
-            image: AssetImage(AssetsImg.test))
-        ),
+    return AspectRatio(aspectRatio: 2.7/4,
+    child: Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage(AssetsImg.test))
       ),
-      ),
+    ),
     );
   }
 }
--- lib/features/home/presentation/views/widget/home_view_body.dart
+++ lib/features/home/presentation/views/widget/home_view_body.dart
@@ -7,15 +7,26 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
-      children: [
-        CustomAppBar(),
-        CustomListViewItem(),
-      ],
-    );
+    return Column(children: [CustomAppBar(),
+    FeatureBookListView()
+    ]);
   }
 }
 
+class FeatureBookListView extends StatelessWidget {
+  const FeatureBookListView({Key? key}) : super(key: key);
 
-
-
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return FeatureListViewItem();
+        },
+      ),
+    );
+  }
+}
