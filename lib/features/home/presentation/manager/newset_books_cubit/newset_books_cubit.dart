e2be55f24d0d6a30b3a835e5f15914667a3e1ce9
Mokassem0 <107583487+Mokassem0@users.noreply.github.com>
2025-10-12 13:06:31 +0300

    newest books cubit done

---
+++ lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -0,0 +1,24 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter_application_1/features/home/data/model/book_model/book_model.dart';
+import 'package:flutter_application_1/features/home/data/repo/home_repo.dart';
+
+part 'newset_books_state.dart';
+
+class NewsetBooksCubit extends Cubit<NewsetBooksState> {
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(NewsetBooksFailure(failure.errMessage));
+      },
+      (books) {
+        emit(NewsetBooksSuccess(books));
+      },
+    );
+  }}
