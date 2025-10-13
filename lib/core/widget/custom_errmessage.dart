65dba62ed0084a06e590bc47698e419908f2ec51
Mokassem0 <107583487+Mokassem0@users.noreply.github.com>
2025-10-13 12:52:39 +0300

    display featured data part1

---
+++ lib/core/widget/custom_errmessage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/utils/font.dart';
+
+class CustomErrmessage extends StatelessWidget {
+  const CustomErrmessage({super.key, required this.errmsg});
+
+  final String errmsg;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text (errmsg,style: Font.textStyle18,);
+  }
+}
--- lib/features/home/presentation/views/widget/featured_book_list_view.dart
+++ lib/features/home/presentation/views/widget/featured_book_list_view.dart
@@ -1,12 +1,19 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/core/widget/custom_errmessage.dart';
+import 'package:flutter_application_1/core/widget/custom_loading_Indicator.dart';
+import 'package:flutter_application_1/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:flutter_application_1/features/home/presentation/views/widget/custom_book_image.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeatureBookListView extends StatelessWidget {
   const FeatureBookListView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return BlocBuilder<FeaturedBooksCubit, FeaturedBooksState>(
+builder: (context, state) {
+  if (state is FeaturedBooksSuccess) {
     return SizedBox(
       height: MediaQuery.of(context).size.height * 0.3,
       child: ListView.builder(
@@ -16,6 +23,13 @@ class FeatureBookListView extends StatelessWidget {
           return CustomBookImage();
         },
       ),
+    );
+  } else if (state is FeaturedBooksFailure) {
+    return Center(child: CustomErrmessage(errmsg: state.errMessage),);
+  } else {
+    return CustomLoadingIndicator();
+  }
+},
     );
   }
 }
