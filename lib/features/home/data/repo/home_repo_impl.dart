3536d3fd51d511d03b35aaf292ed0eaa8c490956
Mokassem0 <107583487+Mokassem0@users.noreply.github.com>
2025-10-14 13:51:12 +0300

    fix issu with book model

--- lib/features/home/data/repo/home_repo_impl.dart
+++ lib/features/home/data/repo/home_repo_impl.dart
@@ -14,7 +14,7 @@ class HomeRepoImpl implements HomeRepo {
     try {
       var data = await apiService.get(
         endPoint:
-            'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Programming',
+            'volumes?Filtering=free-ebooks&Sorting=newest&q=subject:Computer Science',
       );
       List<BookModel> books = [];
       for (var item in data['items']) {
